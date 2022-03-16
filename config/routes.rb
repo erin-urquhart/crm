7bc3ed8c9e78be23981706d0002502beb8864e6b
erin-urquhart <urquhart.erin@gmail.com>
2022-03-16 18:28:23 -0500

    added partial view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  resources :customers, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
